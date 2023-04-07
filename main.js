fba067a5472d63b983825794b89095dae35e09ab
Vlad <vladllen96681@gmail.com>
2023-04-07 22:11:44 +0300

    jik0ol

--- main.js
+++ main.js
@@ -27,4 +27,32 @@ function initMap() {
     });
 };
 
-//
+// NAV BURGER
+const headerNav = document.querySelector('.header__nav');
+const burg = document.createElement('div');
+burg.className = 'burg';
+
+burg.innerHTML = `<div class="menu__btn-burger"></div>`
+
+function resize() {
+    if (window.screen.width <= 992) {
+        if (!headerNav.contains(burg)) {
+            headerNav.appendChild(burg);
+        }
+    } else {
+        if (headerNav.contains(burg)) {
+            headerNav.removeChild(burg);
+        }
+    }
+}
+
+window.addEventListener('resize', resize);
+burg.addEventListener('click', () => {
+    const headerMenu = document.querySelector('.header__menu')
+
+    burg.classList.toggle('active');
+    document.body.classList.toggle('lock');
+    // document.querySelector('.header__logo').classList.toggle('open');
+    headerMenu.classList.toggle('open');
+    // document.querySelector('.header__button').classList.toggle('open');
+})
